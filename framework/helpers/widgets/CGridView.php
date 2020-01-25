d2b3c102231c67a35c8cc367c5ea464edd9e5e23
Samuel Akopyan <>
2020-01-25 13:49:03 +0200

    ~ prepare to release

--- framework/helpers/widgets/CGridView.php
+++ framework/helpers/widgets/CGridView.php
@@ -5,7 +5,7 @@
  * @project ApPHP Framework
  * @author ApPHP <info@apphp.com>
  * @link http://www.apphpframework.com/
- * @copyright Copyright (c) 2012 - 2019 ApPHP Framework
+ * @copyright Copyright (c) 2012 - 2020 ApPHP Framework
  * @license http://www.apphpframework.com/license/
  *
  * PUBLIC (static):			PROTECTED:					PRIVATE (static):		
