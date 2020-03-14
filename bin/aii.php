93d77665a5238dcbc01298779af82247bce8ff48
Samuel Akopyan <>
2020-03-14 14:22:18 +0200

    Added console cache:clear and cache:clear-all command

--- bin/aii.php
+++ bin/aii.php
@@ -33,6 +33,9 @@ function redbg($string = '')
  * --version
  */
 
+//print_r($_SERVER['argc']);
+//print_r($_SERVER['argv']);
+
 $output = '';
 
 if (!empty($argv)) {
@@ -54,7 +57,7 @@ if (!empty($argv)) {
 
             $output .= yellow("Options:") . PHP_EOL;
             $output .= "  ".green("-h, --help")."\t\tDisplay this help message". PHP_EOL;
-            $output .= "  ".green("-V, --version")."\t\tDisplay this application version". PHP_EOL;
+            $output .= "  ".green("-v, --version")."\t\tDisplay this application version". PHP_EOL;
 
             //-q, --quiet           Do not output any message
             //-n, --no-interaction  Do not ask any interactive question
@@ -62,14 +65,57 @@ if (!empty($argv)) {
 
             $output .= yellow("Available commands:") . PHP_EOL;
             $output .= "  ".green("cache:clear")."\t\tFlush specific application cache". PHP_EOL;
-            $output .= "  ".green("cache:clearall")."\tFlush all application cache". PHP_EOL;
+            $output .= "  ".green("cache:clear-all")."\tFlush all application cache". PHP_EOL;
 
             break;
 
-        case '-V':
+        case '-v':
         case '-version':
 
             $output .= 'ApPHP Framework ' . green(A::version());
+
+            break;
+
+        case 'cache:clear':
+        case 'cache:clear-all':
+
+            if ($command === 'cache:clear-all') {
+                $param = 'all';
+            }
+
+            if (empty($param)) {
+                $output .= redbg("No cache type for deleting is defined. Type cache:clear -help") . PHP_EOL;
+            } elseif ($param === '-h' || $param === '-help') {
+                $output .= yellow("Usage:") . PHP_EOL;
+                $output .= "  cache:clear-all\tFlush all application cache". PHP_EOL;
+                $output .= "  cache:clear [type]\tFlush specific application cache". PHP_EOL;
+                $output .= "  \t\t\t[type] - the type of cache to be removed: 'db', 'css', 'js' or 'all'". PHP_EOL;
+            } elseif (in_array($param, array('db', 'css', 'js', 'all'))) {
+                if($param == 'db' || $param == 'all'){
+                    if (CConfig::get('cache.db.path') == '') {
+                        $output .= redbg("Config value 'cache.db.path' is not defined. Check your configuration file.") . PHP_EOL;
+                    }else{
+                        $result = CFile::emptyDirectory(CConfig::get('cache.db.path'), array('index.html'));
+                        $output .= 'DB cache ' . ($result ? 'successfully cleaned' : 'error');
+                    }
+                }
+                if($param == 'css' || $param == 'all'){
+                    if (CConfig::get('compression.css.path') == '') {
+                        $output .= redbg("Config value 'compression.css.path' is not defined. Check your configuration file.") . PHP_EOL;
+                    }else{
+                        $result = CFile::emptyDirectory(CConfig::get('compression.css.path'), array('index.html'));
+                        $output .= 'CSS cache ' . ($result ? 'successfully cleaned' : 'error');
+                    }
+                }
+                if($param == 'js' || $param == 'all'){
+                    if (CConfig::get('compression.js.path') == '') {
+                        $output .= redbg("Config value 'compression.js.path' is not defined. Check your configuration file.") . PHP_EOL;
+                    }else{
+                        $result = CFile::emptyDirectory(CConfig::get('compression.js.path'), array('index.html'));
+                        $output .= 'JS cache ' . ($result ? 'successfully cleaned' : 'error');
+                    }
+                }
+            }
 
             break;
 
